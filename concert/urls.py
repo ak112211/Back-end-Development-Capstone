1f497f01d767c7ef002b7bae718f899bf9683562
Ali H. Khalaj <76651552+ak112211@users.noreply.github.com>
2024-09-13 23:14:23 +0330

    Update urls.py

--- concert/urls.py
+++ concert/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path("", views.logout_view, name="logout"),
     path("", views.signup, name="signup"),
     path("", views.concerts, name="concerts"),
+    path("songs/", views.songs, name="songs"),
+    path("photos/", views.photos, name="photos"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("concert/", views.concerts, name="concerts"),
     path("concert-detail/<int:id>", views.concert_detail, name="concert_detail"),
     path("concert_attendee/", views.concert_attendee, name="concert_attendee"),
 ]
